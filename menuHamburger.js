dec026d2bb0d25d6a33f7268230c4e6143352aca
HAjulien <96483338+HAjulien@users.noreply.github.com>
2021-12-27 21:37:43 +0100

    initial commit

--- menuHamburger.js
+++ menuHamburger.js
@@ -1,6 +1,6 @@
 function toggleMenu () {
     const navbar = document.querySelector('.navbar');
-    const burger = document.querySelector('.burger');
+    const burger = document.querySelector('.hamburger');
     burger.addEventListener('click', (e) => {
     navbar.classList.toggle('open-nav');
     })
